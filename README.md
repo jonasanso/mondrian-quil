84ec7cfc86a579c2fee420fedb6b64b8ef524a15
Jonas Anso <janso@elmar.nl>
2014-12-31 16:34:37 +0000

    Initial version

---
+++ README.md
@@ -0,0 +1,14 @@
+# mondrian-quil
+
+A Quil sketch designed to show a mondrian like drawing.
+
+## Usage
+
+Run `lein compile` command and open `web/index.html` in your browser.
+
+For interactive development run `lein cljsbuild auto` command. This
+command will recompile js file every time you modify `core.cljs`.
+
+## License
+
+Distributed under the Eclipse Public License either version 1.0 or any later version.
---
+++ src/hello_quil/core.cljs
@@ -0,0 +1,67 @@
+(ns mondrian-quil.core
+  (:require [quil.core :as q :include-macros true]
+            [quil.middleware :as m]))
+
+
+(defn setup [])
+
+(defn updatexx [state]
+  ; Update sketch state by changing circle color and position.
+  {:color (mod (+ (:color state) 0.7) 255)
+   :angle (+ (:angle state) 0.1)})
+
+(defn vertical-line []
+  )
+
+(defn draw [state]
+  ; always white background .
+  (q/background 255)
+
+  ; no stroke for squares
+  (q/no-stroke)
+
+  ; red square.
+  (q/fill 255 0 0)
+  (q/rect 50 50 100 100)
+
+  ; blue square.
+  (q/fill 0 0 255)
+  (q/rect 150 200 200 100)
+
+  ; yellow square.
+  (q/fill 255 255 0)
+  (q/rect 150 150 75 50)
+
+  ; black rectangle.
+  (q/fill 0 0 0)
+  (q/rect 150 50 75 50)
+
+  ; let's do some black lines
+  (q/stroke 0 0 0)
+  (q/stroke-weight 5)
+
+  (q/line 50 0 50 300)
+  (q/line 150 50 150 300)
+  (q/line 225 0 225 200)
+  (q/line 100 150 100 200)
+
+  (q/line 0 50 345 50)
+  (q/line 150 50 220 50)
+  (q/line 0 150 345 150)
+  (q/line 0 200 345 200)
+  (q/line 0 250 150 250)  )
+
+(q/defsketch mondrian-quil
+  :host "mondrian-quil"
+  :size [500 300]
+  ; setup function called only once, during sketch initialization.
+  :setup setup
+  ; update is called on each iteration before draw is called.
+  ; It updates sketch state.
+  ;:update update
+  :draw draw
+  ; This sketch uses functional-mode middleware.
+  ; Check quil wiki for more info about middlewares and particularly
+  ; fun-mode.
+  ;:middleware [m/fun-mode]
+  )
