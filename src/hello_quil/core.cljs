e70acc350e8cfdace216cba1ba773a5a3a8a1a97
Jonas Anso <janso@elmar.nl>
2015-01-16 20:00:53 +0000

    Define the squares and the lines

--- src/hello_quil/core.cljs
+++ src/hello_quil/core.cljs
@@ -2,44 +2,42 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
+(def squares
+  [ {:color [255 0 0]
+     :rect [50 50 100 100]}
+    {:color [0 0 255]
+     :rect [150 200 200 100]}
+    {:color [255 255 0]
+     :rect [150 150 75 50]}
+    {:color [0 0 0]
+     :rect [150 50 75 50]} ])
 
-(defn draw [state]
+(def lines
+  [ [50 0 50 300]
+    [150 50 150 300]
+    [225 0 225 200]
+    [100 150 100 200]
+    [0 50 345 50]
+    [150 50 220 50]
+    [0 150 345 150]
+    [0 200 345 200]
+    [0 250 150 250] ])
+
+(defn draw []
   ; always white background .
   (q/background 255)
 
   ; no stroke for squares
   (q/no-stroke)
-
-  ; red square.
-  (q/fill 255 0 0)
-  (q/rect 50 50 100 100)
-
-  ; blue square.
-  (q/fill 0 0 255)
-  (q/rect 150 200 200 100)
-
-  ; yellow square.
-  (q/fill 255 255 0)
-  (q/rect 150 150 75 50)
-
-  ; black rectangle.
-  (q/fill 0 0 0)
-  (q/rect 150 50 75 50)
+  (doseq [s squares]
+    (apply q/fill (:color s))
+    (apply q/rect (:rect s)))
 
   ; let's do some black lines
   (q/stroke 0 0 0)
   (q/stroke-weight 5)
-
-  (q/line 50 0 50 300)
-  (q/line 150 50 150 300)
-  (q/line 225 0 225 200)
-  (q/line 100 150 100 200)
-
-  (q/line 0 50 345 50)
-  (q/line 150 50 220 50)
-  (q/line 0 150 345 150)
-  (q/line 0 200 345 200)
-  (q/line 0 250 150 250))
+  (doseq [line lines]
+    (apply q/line line)))
 
 (q/defsketch mondrian-quil
   :host "mondrian-quil"
